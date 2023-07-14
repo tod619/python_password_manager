8068602f9373106f8d1f4a69934dffc183fdb4de
tod619 <trevorodwyer619@hotmail.com>
2023-07-14 21:39:49 +0100

    Refactor json code to updata & appened code to file in main.py

--- main.py
+++ main.py
@@ -48,9 +48,16 @@ def save():
         messagebox.showinfo(
             title="OOPS", message="Please don't leave any of the fileds empty!")
     else:
-        with open("data.json", "w") as data_file:
+        with open("data.json", "r") as data_file:
             # data_file.write(f"{website} | {email} | {password}\n")
-            json.dump(new_data, data_file, indent=4)
+            # Reading the old data
+            data = json.load(data_file)
+            # Update the old dat
+            data.update(new_data)
+
+        with open("data.json", "w") as data_file:
+            # saving the updated data to the json file
+            json.dump(data, data_file, indent=4)
             website_entry.delete(0, END)
             password_entry.delete(0, END)
 
