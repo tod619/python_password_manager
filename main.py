ef802ca7117017bb1ff002e3a2fcb29852726a45
tod619 <trevorodwyer619@hotmail.com>
2023-07-16 21:48:54 +0100

    Add search btn to UI in main.py file

--- main.py
+++ main.py
@@ -90,10 +90,10 @@ password_label = Label(text="Password:")
 password_label.grid(row=3, column=0)
 
 # Entries
-website_entry = Entry(width=35)
-website_entry.grid(row=1, column=1, columnspan=2)
+website_entry = Entry(width=21)
+website_entry.grid(row=1, column=1)
 website_entry.focus()
-email_entry = Entry(width=35)
+email_entry = Entry(width=30)
 email_entry.grid(row=2, column=1, columnspan=2)
 email_entry.insert(0, 'myemail@gmail.com')
 password_entry = Entry(width=21)
@@ -106,5 +106,8 @@ generate_password_button.grid(row=3, column=2)
 
 add_button = Button(text="Add", width=36, command=save)
 add_button.grid(row=4, column=1, columnspan=2)
+
+search_button = Button(text="search", width=13)
+search_button.grid(row=1, column=2)
 
 window.mainloop()
